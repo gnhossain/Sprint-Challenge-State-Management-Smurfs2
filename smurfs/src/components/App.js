c75f0ea2f6615f2c795d05a325bcd1d27bfced5a
gul hossain <spicymangojungle@gmail.com>
2020-03-20 09:10:05 -0700

    app

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,5 +1,10 @@
 import React, { Component } from "react";
 import "./App.css";
+
+import 'semantic-ui-css/semantic.min.css'
+
+import SmurfList from './SmurfList';
+import SmurfForm from './SmurfForm';
 class App extends Component {
   render() {
     return (
@@ -8,6 +13,8 @@ class App extends Component {
         <div>Welcome to your state management version of Smurfs!</div>
         <div>Start inside of your `src/index.js` file!</div>
         <div>Have fun!</div>
+        <SmurfForm/>
+        <SmurfList/>
       </div>
     );
   }
